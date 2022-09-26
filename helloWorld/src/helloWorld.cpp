49380212502d136fcfdf566c1dd76ee69a5e559d
aaron GitHub <aaronse@outlook.com>
2022-09-25 21:54:27 -0700

    Update HelloWorld example to configTime in Amsterdam timezone

--- helloWorld/src/helloWorld.cpp
+++ helloWorld/src/helloWorld.cpp
@@ -5,6 +5,7 @@
     #define LittleFS LITTLEFS
 #elif defined(ESP8266)
     #include "LittleFS.h"
+    #include <TZ.h>
 #endif
 
 #include "WiFiManager.h"
@@ -13,6 +14,7 @@
 #include "fetch.h"
 #include "configManager.h"
 #include "timeSync.h"
+
 
 void setup() 
 {
@@ -26,6 +28,48 @@ void setup()
     GUI.begin();
 
     Serial.println("Hello world, setup");
+
+    // Configure device time using NTP server, with Amsterdam timezone
+#ifdef ESP32
+    // ESP32 sntp APIs expect posix based time zone expressions, see https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
+    timeSync.begin(PSTR("CET-1CEST,M3.5.0,M10.5.0/3"), "pool.ntp.org");
+#elif defined(ESP8266)
+    timeSync.begin(TZ_Europe_Amsterdam);
+#endif
+
+#ifdef ESP32
+    // Two example approaches for looking up current time... 
+
+    // Example 1, fill tm struct using getLocalTime.  Accessing time parts explicitly.
+    struct tm timeinfo;
+    if(!getLocalTime(&timeinfo)){
+        Serial.println("Failed to obtain time");
+        return;
+    }
+    Serial.println(&timeinfo, "Current time in Amsterdam: %A, %B %d %Y %H:%M:%S");
+
+    // Example 2, use asctime to serialize local time to string
+    time_t now = time(nullptr);
+    Serial.print(PSTR("Current time in Amsterdam: "));
+    Serial.print(asctime(localtime(&now)));
+    
+
+#elif defined(ESP8266)
+    //Wait for connection
+    timeSync.waitForSyncResult(10000);
+
+    if (timeSync.isSynced())
+    {
+        time_t now = time(nullptr);
+        Serial.print(PSTR("Current time in Amsterdam: "));
+        Serial.print(asctime(localtime(&now)));
+    }
+    else 
+    {
+        Serial.println("Timeout while receiving the time");
+    }
+
+#endif
 }
 
 void loop() 
